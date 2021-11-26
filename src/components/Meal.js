080f94e7af0f18676facd3c7c80e0d6de65af92a
Alaska01 <aadaniel108@gmail.com>
2021-11-26 15:18:56 +0100

    NavBar component created

--- src/components/Meal.js
+++ src/components/Meal.js
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import '../styles/Meal.css';
 
 const Meal = ({ id, name, image }) => (
   <div className="meal-card " data-testid="meal-card" key={id}>

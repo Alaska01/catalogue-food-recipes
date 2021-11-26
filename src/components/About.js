96d2125dc6d0bfe4645715889b4a10e2a2078ff7
Alaska01 <aadaniel108@gmail.com>
2021-11-26 16:18:34 +0100

    Fix styling issues

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,16 @@ const About = () => (
   <div className="about-us">
     <h1 className="about">About Us</h1>
     <p>
-      Delicious Yummy Kitchen- Romeos Pizza Nigeria
-      No food can make you look younger and feel healthier overnight. 
-      But over time, getting the right nutrients can make a difference. 
-      Here's the scoop on five superfoods that can help you lose weight, 
-      boost your heart health and give your skin a healthy glow.
+      Delicious Yummy Kitchen- Romeos Pizza. No food can make you look.
+      Younger and feel healthier overnight. But over time, getting the
+      right nutrients can make a difference. Our five times scoop superfoods
+      that can help you lose weight, boost your heart health
+      and give your skin a healthy glow.
     </p>
     <p>
-    You've heard the old saying: Real beauty comes from the inside. 
-    You could say the same for good health. When you eat right, exercise, get enough sleep.
-    You can visit us, we have nutitionist that can help you out with your needs.
+      Have you heard the the old saying: Real beauty comes from the inside.
+      You could say the same for good health. When you eat right, exercise, get enough sleep.
+      You can visit us, we have nutitionist that can help you out with your needs.
     </p>
   </div>
 );
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import MealList from '../containers/MealList';
+import MealDetails from '../containers/MealDetails';
+import Error from './Error';
+import About from './About';
 
 function App() {
   return (
-    <div>
-      <h1>Reconfiguring Project After Persistent errors for days</h1>
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<MealList />} />
+          <Route path="/Meal/:id" element={<MealDetails />} />
+          <Route exact path="About" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

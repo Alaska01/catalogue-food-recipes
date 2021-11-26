69ee40e3d7b0c72708f244544f1b4f760b509a31
Alaska01 <aadaniel108@gmail.com>
2021-11-26 11:16:01 +0100

    Create About Page Component

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const About = () => (
+  <div className="about-us">
+    <h1 className="about">About Us</h1>
+    <p>
+      Delicious Yummy Kitchen- Romeos Pizza Nigeria
+      No food can make you look younger and feel healthier overnight. 
+      But over time, getting the right nutrients can make a difference. 
+      Here's the scoop on five superfoods that can help you lose weight, 
+      boost your heart health and give your skin a healthy glow.
+    </p>
+    <p>
+    You've heard the old saying: Real beauty comes from the inside. 
+    You could say the same for good health. When you eat right, exercise, get enough sleep.
+    You can visit us, we have nutitionist that can help you out with your needs.
+    </p>
+  </div>
+);
+
+export default About;
